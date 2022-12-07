0382b3a32e53200a2b15244aa2904033e0eff0b9
Davide <davide.ceschia@gmail.com>
2022-12-07 16:42:59 +0100

    remove nonsense

--- day5/second/src/main.rs
+++ day5/second/src/main.rs
@@ -11,22 +11,14 @@ fn main() {
 }
 
 fn do_work(input: &[u8]) {
-    let lookup: &mut [u8] = &mut [0; MARKER_LENGTH];
-
     'outer: for (index, window) in input.windows(MARKER_LENGTH).enumerate() {
-
         for (sub_index, byte) in window.iter().enumerate() {
-            if lookup[..sub_index].contains(byte) {
+            if window[..sub_index].contains(byte) {
                 continue 'outer;
-            } else {
-                lookup[sub_index] = *byte;
             }
         }
-
-        if lookup.len() == MARKER_LENGTH {
-            println!("Found: {:?}", index + MARKER_LENGTH);
-            break;
-        }
+        println!("Found: {:?}", index + MARKER_LENGTH);
+        return;
     }
 }
 
